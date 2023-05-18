5c1c34874c3a775d2bd98bc8eba65717d69f37c1
Luciano van der Veekens <lvanderveekens@proton.me>
2023-05-18 16:07:22 +0200

    Styles

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -25,37 +25,45 @@ export default function HomePage() {
 
   return (
     <main>
-      <nav className="py-4 mb-4">
-        <div className="container text-xl text-white flex justify-between align-center">
-          <div className="text-2xl font-bold self-center">LanguageQuiz</div>
-        </div>
-      </nav>
+      <div id="hero">
+        <nav className="py-4 b-4">
+          <div className="container text-xl text-white flex justify-between align-center">
+            <div className="text-2xl font-bold self-center">LanguageQuiz</div>
+          </div>
+        </nav>
 
-      <div className="container mx-auto text-white">
-        <div className="text-center my-40">
-          <div className="text-3xl font-bold mb-8">
-            Language Quizzes Made Easy: Empowering Students, Supporting Teachers.
+        <div className="text-center">
+          <div className="container mx-auto text-white">
+            <div className="py-40">
+              <div className="text-3xl font-bold mb-8">
+                Language Quizzes Made Easy: Empowering Students, Supporting
+                Teachers.
+              </div>
+              <div>
+                <button
+                  className="text-black font-bold text-xl bg-white border-2 border-white px-4 py-2 rounded-lg px-3 mb-4"
+                  onClick={() => router.push("#latest-quizzes")}
+                >
+                  Take a quiz
+                </button>
+              </div>
+              <div className="">
+                <button
+                  className="text-xl font-bold px-4 py-2 border-2 border-white rounded-lg px-3"
+                  onClick={() => router.push("/create-quiz")}
+                >
+                  Create a quiz
+                </button>
+              </div>
+            </div>
           </div>
-          <div>
-            <button
-              className="text-xl bg-[#0066FF] border-2 border-[#0066FF] px-4 py-2 rounded-lg px-3 mb-4"
-              onClick={() => router.push("#latest-quizzes")}
-            >
-              Take a quiz
-            </button>
-          </div>
-          <div className="">
-            <button
-              className="text-xl px-4 py-2 border-2 border-white rounded-lg px-3"
-              onClick={() => router.push("/create-quiz")}
-            >
-              Create a quiz
-            </button>
-          </div>
-       </div> 
-        <div id="latest-quizzes" className="my-24">
-          <h2 className="text-white text-2xl font-bold mb-8">Latest Quizzes</h2>
-          <div className="grid grid-cols-3 gap-4">
+        </div>
+      </div>
+
+      <div className="container mx-auto text-black">
+        <div id="latest-quizzes" className="my-8">
+          <h2 className="text-2xl font-bold mb-8">Latest Quizzes</h2>
+          <div className="grid grid-cols-4 gap-4">
             {quizzes.length > 0 &&
               quizzes
                 .sort((a, b) => {
@@ -67,7 +75,7 @@ export default function HomePage() {
                   return (
                     <div key={quiz.id}>
                       <Link href={`/quizzes/${quiz.id}`}>
-                        <div className="bg-white text-black relative rounded-lg aspect-[3/2]">
+                        <div className="bg-white border border-2 border-black text-black relative rounded-lg aspect-[3/2]">
                           <div className="px-4 py-4">
                             <div>
                               <span className="font-bold">Name:</span>{" "}
