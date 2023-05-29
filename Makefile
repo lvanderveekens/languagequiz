796190d716d3e1234152b113aef59db88b6338e6
Luciano van der Veekens <lvanderveekens@proton.me>
2023-05-29 21:07:24 +0200

    Google analytics

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
 ssh: 
 	ssh root@161.35.247.132
-
-scp-frontend-dotenv:
-	scp frontend/.env.prd root@161.35.247.132:~/languagequiz/frontend/.env
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
