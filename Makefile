366edb6ca177cd8f758d0beacaaddcef25ac658b
Luciano van der Veekens <lvanderveekens@proton.me>
2023-05-30 16:26:25 +0200

    changes

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 ssh: 
 	ssh root@161.35.247.132
+
+port-forward-db:
+	ssh -L 25432:localhost:5432 -Nf root@161.35.247.132
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,15 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!, {
-        page_path: url,
-      });
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!, { page_path: url });
     };
     router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
@@ -18,5 +17,27 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
